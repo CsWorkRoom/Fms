592f45fa6196704066ec43a56618b3dc272fc025
chengsen <419746405@qq.com>
2018-01-22 11:23:02 +0800

    常量

---
+++ Em.Project.Core/Domain/Monit/MonitConst.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Easyman.Domain
+{
+    public class MonitConst
+    {
+        /// <summary>
+        /// 还原特殊字符串
+        /// </summary>
+        public const string RestoreStr = "abcdefg$$lcz&&cs";//命名规则后面可以调整
+
+        //其他可能需要排除的字符串定义
+    }
+}
